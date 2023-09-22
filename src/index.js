2ddecc2fa2b3e31b4a9e5604c89b9c699e494b70
pemaismais <pedrodelafacul@gmail.com>
2023-09-22 18:40:02 -0300

    Menu dropdown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-function menuCor() {
-    document.getElementById("menuCor").style.display = 'block'
+function changeBackgroundColor() {
+    let backgroundColorOption = document.getElementById("backgroundColorOption");
+    backgroundColorOption.style.display = backgroundColorOption.style.display === "none" ? "flex" : "none";
+
+    let menuDrop = document.getElementById("optionsDrop");
+    menuDrop.style.display = menuDrop.style.display = "none";
 }
+
+
+function displayMenuDrop(){
+    let menudrop = document.getElementById("menuDrop");
+    menudrop.style.display = menudrop.style.display === "none" ? "block" : "none";
+    
+    let menuDrop = document.getElementById("optionsDrop");
+    menuDrop.style.display = menuDrop.style.display = "block";
+
+    let optionsDrop = document.getElementById("backgroundColorOption");
+    optionsDrop.style.display = optionsDrop.style.display = "none";
+}
+  
