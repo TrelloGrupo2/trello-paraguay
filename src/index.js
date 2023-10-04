05e9dfbefd6bd89ddce0b48eae57dc78ab0a2079
pemaismais <pedrodelafacul@gmail.com>
2023-10-04 19:40:46 -0300

    selected theme check

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 function changeBackgroundColorOption() {
   let backgroundColorOption = document.getElementById("backgroundColorOption");
+  
   backgroundColorOption.style.display =
-    backgroundColorOption.style.display === "none" ? "flex" : "none";
+  backgroundColorOption.style.display === "none" ? "flex" : "none";
 
   let optionsDrop = document.getElementById("optionsDrop");
   optionsDrop.style.display = optionsDrop.style.display = "none";
@@ -19,8 +20,9 @@ function selectThemeOption() {
   optionsDrop.style.display = optionsDrop.style.display = "none";
 
   let backgroundColorOption = document.getElementById("backgroundColorOption");
-  backgroundColorOption.style.display = backgroundColorOption.style.display =
-    "none";
+  backgroundColorOption.style.display = backgroundColorOption.style.display ="none";
+  
+
 }
 
 function displayMenuDrop() {
@@ -31,8 +33,7 @@ function displayMenuDrop() {
   optionsDrop.style.display = optionsDrop.style.display = "block";
 
   let backgroundColorOption = document.getElementById("backgroundColorOption");
-  backgroundColorOption.style.display = backgroundColorOption.style.display =
-    "none";
+  backgroundColorOption.style.display = backgroundColorOption.style.display = "none";
 
   let selectThemeOption = document.getElementById("selectThemeOption");
   selectThemeOption.style.display = selectThemeOption.style.display = "none";
@@ -40,15 +41,27 @@ function displayMenuDrop() {
 
 function changingBackgroundColor() {
   let colorInput = document.querySelector(".colorInput").value;
-  let body = document.getElementById("body");
+  let body = document.getElementById("content");
   body.style.backgroundColor = colorInput;
 }
 
 function changingThemeToDark() {
+  const lightCheck = document.getElementById("lightCheck");
+  const darkCheck = document.getElementById("darkCheck");
   let darkTheme = document.getElementById("darkTheme");
+  
   document.body.classList.add("dark");
+  lightCheck.style.display = lightCheck.style.display = "none";
+  darkCheck.style.display = darkCheck.style.display = "flex";
 }
+
+
 function changingThemeToLight() {
+  const lightCheck = document.getElementById("lightCheck");
+  const darkCheck = document.getElementById("darkCheck");
   let darkTheme = document.getElementById("darkTheme");
+
   document.body.classList.remove("dark");
+  lightCheck.style.display = lightCheck.style.display = "flex";
+  darkCheck.style.display = darkCheck.style.display = "none";
 }
